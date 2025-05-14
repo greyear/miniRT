6d2cea62024f33a173f38f7c8b5b4f667202e090
msavelie <msavelie@student.hive.fi>
2025-05-14 16:44:50 +0300

    Change functions for norminette.

--- src/drawing/calculate_colors.c
+++ src/drawing/calculate_colors.c
@@ -6,7 +6,7 @@
 /*   By: msavelie <msavelie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 14:49:50 by msavelie          #+#    #+#             */
-/*   Updated: 2025/04/24 14:51:05 by msavelie         ###   ########.fr       */
+/*   Updated: 2025/05/14 11:40:55 by msavelie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 uint32_t	vec_to_rgba(t_vector color)
 {
-	uint8_t	r = (uint8_t)(fmax(0, fmin(1, color.x)) * 255);
-	uint8_t	g = (uint8_t)(fmax(0, fmin(1, color.y)) * 255);
-	uint8_t	b = (uint8_t)(fmax(0, fmin(1, color.z)) * 255);
-	uint8_t	a = 255;
+	uint8_t	r;
+	uint8_t	g;
+	uint8_t	b;
+	uint8_t	a;
 
+	r = (uint8_t)(fmax(0, fmin(1, color.x)) * 255);
+	g = (uint8_t)(fmax(0, fmin(1, color.y)) * 255);
+	b = (uint8_t)(fmax(0, fmin(1, color.z)) * 255);
+	a = 255;
 	return ((a << 24) | (r << 16) | (g << 8) | b);
 }
