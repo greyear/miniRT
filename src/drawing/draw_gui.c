f7f5a42d54037d95470ee5112c2f7b7357e5f03f
msavelie <msavelie@student.hive.fi>
2025-05-17 18:15:59 +0300

    Specify object type in selection.

--- src/drawing/draw_gui.c
+++ src/drawing/draw_gui.c
@@ -6,7 +6,7 @@
 /*   By: msavelie <msavelie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 12:19:49 by msavelie          #+#    #+#             */
-/*   Updated: 2025/05/17 16:26:29 by msavelie         ###   ########.fr       */
+/*   Updated: 2025/05/17 18:12:55 by msavelie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ static void	draw_back(mlx_image_t *image)
 	uint32_t	y;
 
 	y = 0;
-	while (y < 45)
+	while (y < 50)
 	{
 		x = 0;
-		while (x < 80)
+		while (x < 130)
 		{
 			mlx_put_pixel(image, x, y, 0x000000FF);
 			x++;
@@ -30,24 +30,38 @@ static void	draw_back(mlx_image_t *image)
 	}
 }
 
-static void	draw_current_obj(t_rt *rt, mlx_t *obj)
+static char	*check_obj_type(t_rt *rt)
 {
 	char	*full_str;
 	char	*cur_index;
 
+	full_str = NULL;
+	cur_index = ft_itoa(rt->obj_index + 1);
+	if (!cur_index)
+		clean_exit(rt);
+	if (rt->objects[rt->obj_index].type == SPHERE)
+		full_str = ft_strjoin("sphere: ", cur_index);
+	else if (rt->objects[rt->obj_index].type == CYLINDER)
+		full_str = ft_strjoin("cylinder: ", cur_index);
+	else if (rt->objects[rt->obj_index].type == PLANE)
+		full_str = ft_strjoin("plane: ", cur_index);
+	if (!full_str)
+	{
+		free(cur_index);
+		clean_exit(rt);
+	}
+	free(cur_index);
+	return (full_str);
+}
+
+static void	draw_current_obj(t_rt *rt, mlx_t *obj)
+{
+	char	*full_str;
+
 	mlx_delete_image(obj, rt->instr_img);
 	if (rt->obj_sel == OBJ_SEL)
 	{
-		cur_index = ft_itoa(rt->obj_index);
-		if (!cur_index)
-			clean_exit(rt);
-		full_str = ft_strjoin("obj: ", cur_index);
-		if (!full_str)
-		{
-			free(cur_index);
-			clean_exit(rt);
-		}
-		free(cur_index);
+		full_str = check_obj_type(rt);
 		rt->instr_img = mlx_put_string(obj, full_str, 5, 5);
 		free(full_str);
 	}
@@ -71,7 +85,7 @@ static void	draw_current_mode(t_rt *rt, mlx_t *obj)
 void	draw_gui(t_rt *rt)
 {
 	mlx_delete_image(rt->mlx, rt->gui_img);
-	rt->gui_img = mlx_new_image(rt->mlx, 80, 45);
+	rt->gui_img = mlx_new_image(rt->mlx, 130, 50);
 	draw_back(rt->gui_img);
 	mlx_image_to_window(rt->mlx, rt->gui_img, 0, 0);
 	draw_current_obj(rt, rt->mlx);
