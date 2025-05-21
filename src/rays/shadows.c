83dfa7cebf687cc93275c344402c7f7c6439178c
msavelie <msavelie@student.hive.fi>
2025-05-21 20:21:01 +0300

    Add break case with some fix attempts.

--- src/rays/shadows.c
+++ src/rays/shadows.c
@@ -6,33 +6,66 @@
 /*   By: msavelie <msavelie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 14:24:35 by msavelie          #+#    #+#             */
-/*   Updated: 2025/05/21 17:35:22 by msavelie         ###   ########.fr       */
+/*   Updated: 2025/05/21 20:14:45 by msavelie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/mini_rt.h"
 
+// static bool	set_plane_shadow(t_ray light_ray, t_rt *rt,
+// 	t_hit *hit_info, t_obj object)
+// {
+// 	float		t;
+// 	bool		shadow_hit;
+// 	t_vector	hit_point;
+// 	t_vector	light_to_hit;
+// 	t_vector	point_to_hit;
+
+// 	shadow_hit = false;
+// 	if (intersect_plane(light_ray, object, &t) && t > BIAS)
+// 	{
+// 		hit_point = vec_add(light_ray.orig, vec_mul_num(light_ray.dest, t));
+// 		light_to_hit = vec_sub(hit_point, rt->light.coords);
+// 		point_to_hit = vec_sub(hit_point, hit_info->phit);
+// 		if (dot(light_to_hit, object.normalized)
+// 			* dot(point_to_hit, object.normalized) < 0)
+// 		{
+// 			hit_info->t0 = t;
+// 			shadow_hit = true;
+// 		}
+// 	}
+// 	return (shadow_hit);
+// }
+
+static float vec_length(t_vector v)
+{
+	return (sqrt(v.x * v.x + v.y * v.y + v.z * v.z));
+}
+
 static bool	set_plane_shadow(t_ray light_ray, t_rt *rt,
 	t_hit *hit_info, t_obj object)
 {
 	float		t;
+	float		light_dist;
 	bool		shadow_hit;
-	t_vector	hit_point;
-	t_vector	light_to_hit;
-	t_vector	point_to_hit;
+	float		dist_from_light;
+	//t_vector	light_dir;
 
 	shadow_hit = false;
-	if (intersect_plane(light_ray, object, &t) && t > BIAS)
+	dist_from_light = dot(vec_sub(rt->light.coords, object.coords), object.normalized);
+	if (fabs(dist_from_light) < 1e-4)
+		return false;
+
+	// Новый блок: проверить, находится ли свет на "другой стороне" плоскости
+	t_vector hit_to_light = vec_sub(rt->light.coords, hit_info->phit);
+	if (dot(hit_to_light, object.normalized) < 0)
+		return false;
+
+	light_dist = vec_length(hit_to_light);
+	if (intersect_plane(light_ray, object, &t) && t > BIAS && t < light_dist)
 	{
-		hit_point = vec_add(light_ray.orig, vec_mul_num(light_ray.dest, t));
-		light_to_hit = vec_sub(hit_point, rt->light.coords);
-		point_to_hit = vec_sub(hit_point, hit_info->phit);
-		if (dot(light_to_hit, object.normalized)
-			* dot(point_to_hit, object.normalized) < 0)
-		{
-			hit_info->t0 = t;
-			shadow_hit = true;
-		}
+		hit_info->t0 = t;
+		shadow_hit = true;
 	}
 	return (shadow_hit);
 }
